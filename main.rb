b532448a9cf665e0eae0fa67a22cfe0f922e72f0
alvinkenyagah <alvin47k@gmail.com>
2023-05-28 11:14:04 +0300

    Add initial project files

---
+++ main.rb
@@ -0,0 +1,15 @@
+require_relative './lib/author'
+require_relative './lib/article'
+require_relative './lib/magazine'
+
+
+jon = Author.new('Jon')
+# magazine = Magazine.new('Fashion Magazine', 'Fashion')
+
+
+puts jon.name
+# puts magazine.name
+# puts magazine.category
+
+
+
